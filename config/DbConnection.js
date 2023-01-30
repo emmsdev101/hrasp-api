1595f32a4280b43aa4a83e0d116e5999366f87d0
emmsdev101 <emman199810@example.com>
2023-01-31 02:51:51 +0800

    Change connector to mysql2

--- config/DbConnection.js
+++ config/DbConnection.js
@@ -1,7 +1,7 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
 exports.con = mysql.createConnection({
-  host: "18.191.54.215",
+  host: "localhost",
   user: "hrasp",
   password:"1234",
   database:"hrasp"
