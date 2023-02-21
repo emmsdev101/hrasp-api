6c3557e97d41e91b7c1d8bc39ba1d5c37bd12f49
emmsdev101 <emman199810@gmail.com>
2023-02-21 19:15:49 +0800

    Removed notification

--- helper/smsSender.js
+++ helper/smsSender.js
@@ -2,9 +2,9 @@ const { default: axios } = require("axios")
 
 exports.sendSms = async(reciever, message)=>{
 
-    axios.get(`http://localhost/sms-api?number=${reciever}&message=${message}`).then((result)=>{
-      console.log("Message sent",result.data)
-    })
+    // axios.get(`http://localhost/sms-api?number=${reciever}&message=${message}`).then((result)=>{
+    //   console.log("Message sent",result.data)
+    // })
 
     return 0
 
